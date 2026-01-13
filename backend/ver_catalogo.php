4f5a8326578787775d9c74bf80b33477cce5b608
Aleee009 <agamez@campuscamarafp.com>
2026-01-13 10:52:59 +0100

    cambio en el favicon

--- backend/ver_catalogo.php
+++ backend/ver_catalogo.php
@@ -14,7 +14,7 @@ $result = $conn->query($sql);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Catálogo - Retrogroove</title>
   <link rel="stylesheet" href="../frontend/styles.css">
-  <link rel="icon" type="../frontend/image/png" href="img/favicon_o.svg">
+  <link rel="icon" type="../frontend/image/png" href="../frontend/img/favicon_o.svg">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
